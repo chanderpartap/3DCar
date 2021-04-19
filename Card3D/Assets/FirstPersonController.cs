4b550eb6ede99b361fa6f57592e7cc4e42744d33
Chander Partap Singh <partap.pathania@gmail.com>
2021-04-19 22:20:55 +0530

    Mouse Input using Firs Person Controller Added

---
+++ Card3D/Assets/FirstPersonController.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    private static FirstPersonController sharedInstance;
+    [Header("Mouse Lock")]
+    public bool isMouseLocked;
+    [Header("Camera Field of View")]
+    public bool isFieldOfViewEnabled;
+    public float cameraFieldOfViewMin;
+    public float cameraFieldOfViewMax;
+    public float fieldOfViewIncrement;
+    [Header("Camera Rotate X Setting")]
+    public float cameraRotateMax;
+    public float cameraRotateMin;
+    [Header("Mouse Smoothing Value")]
+    public float mouseSmooth;
+    public float speed;
+
+
+    private Camera m_camera;
+    private float m_fieldOfView;
+    private Transform m_parent;
+    private float m_mouseX;
+    private float m_mouseY;
+    private float m_rotateX;
+    private float m_mouseScroll;
+    private Rigidbody rb;
+
+    Vector3 Vec;
+    private void Awake()
+    {
+        sharedInstance = this;
+        m_camera = Camera.main;
+        m_parent = transform.parent;
+        rb = m_parent.GetComponent<Rigidbody>();
+        if(m_camera != null)
+        {
+            m_fieldOfView = m_camera.fieldOfView;
+        }
+        MouseLock();
+    }
+
+    public static FirstPersonController GetSharedInstance()
+    {
+        return sharedInstance;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseInputs();
+        RotatePlayerY();
+        CameraRotateX();
+        CameraZoom();
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(0, 0, 100);
+        }
+        MoveForward();
+
+    }
+
+    private void MouseInputs()
+    {
+        m_mouseX = Input.GetAxis("Mouse X") * mouseSmooth;
+        m_mouseY = Input.GetAxis("Mouse Y") * mouseSmooth;
+        m_mouseScroll = Input.GetAxis("Mouse ScrollWheel");
+    }
+
+    private void RotatePlayerY()
+    {
+        m_parent.Rotate(Vector3.up * m_mouseX);
+    }
+    private void CameraRotateX()
+    {
+        m_rotateX += m_mouseY;
+        m_rotateX = Mathf.Clamp(m_rotateX, cameraRotateMin, cameraRotateMax);
+        m_camera.transform.localRotation = Quaternion.Euler(-m_rotateX, 0f, 0f);
+    }
+    private void MouseLock()
+    {
+        if (isMouseLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            return;
+        }
+        Cursor.lockState = CursorLockMode.None;
+    }
+    private void CameraZoom()
+    {
+        if (isFieldOfViewEnabled)
+        {
+            if (m_mouseScroll > 0.0f)
+            {
+                if(m_fieldOfView + fieldOfViewIncrement >= cameraFieldOfViewMin &&
+                    m_fieldOfView + fieldOfViewIncrement <= cameraFieldOfViewMax)
+                {
+                    m_fieldOfView += fieldOfViewIncrement;
+                    m_camera.fieldOfView = m_fieldOfView;
+                }
+            }
+            if (m_mouseScroll < 0.0f)
+            {
+                if (m_fieldOfView - fieldOfViewIncrement >= cameraFieldOfViewMin &&
+                    m_fieldOfView - fieldOfViewIncrement <= cameraFieldOfViewMax)
+                {
+                    m_fieldOfView -= fieldOfViewIncrement;
+                    m_camera.fieldOfView = m_fieldOfView;
+                }
+            }
+        }
+    }
+    private void MoveForward()
+    {
+        
+    }
+}
