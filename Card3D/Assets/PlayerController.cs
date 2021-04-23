c316ac9925adc17adce9ec69e69cf567e5710a6d
Chander Partap Singh <partap.pathania@gmail.com>
2021-04-23 18:31:40 +0530

    GroundChecked

--- Card3D/Assets/PlayerController.cs
+++ Card3D/Assets/PlayerController.cs
@@ -6,17 +6,31 @@ public class PlayerController : MonoBehaviour
 {
     // Update is called once per frame
     public CharacterController controller;
-    public float speed = 10f;
+    public float speed = 15f;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.3f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            speed = 15f;
+            speed = 20f;
             //Debug.Log(speed);
         }
         else
         {
-            speed = 10f;
+            speed = 15f;
             //Debug.Log(speed);
         }
         float x = Input.GetAxis("Horizontal");
@@ -25,5 +39,7 @@ public class PlayerController : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z ;
 
         controller.Move(move * speed * Time.deltaTime);
+        velocity.y += gravity + Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
